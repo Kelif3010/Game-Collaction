4f96383acc726bc7d40761a157cafc15e468bcda
Keno <elfiandken@icloud.com>
2025-12-27 12:32:07 +0100

    Initial Commit

---
+++ Games Collection/Games_CollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Games_CollectionApp.swift
+//  Games Collection
+//
+//  Created by Ken  on 27.12.25.
+//
+
+import SwiftUI
+
+@main
+struct Games_CollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
